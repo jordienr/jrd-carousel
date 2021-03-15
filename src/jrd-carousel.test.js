eb38a4eb0c2e29770b6a7175d4160511adbb3c89
Jordi <jeroig@idonia.com>
2021-03-15 17:23:09 +0100

    Add test for mounting component, buttons and click events

--- src/jrd-carousel.test.js
+++ src/jrd-carousel.test.js
@@ -28,3 +28,48 @@ test('Passing correct options works', () => {
   } 
   expect(f).not.toThrow()
 })
+
+test('Calling JrdCarousel mounts the carousel', () => {
+  JrdCarousel('carousel', GLOBAL_OPTIONS)
+  const jrdCarousel = document.querySelector('.jrdc-container')
+  expect(jrdCarousel).toBeTruthy()
+})
+
+test('First image is shown after mounting the component', () => {
+  JrdCarousel('carousel', GLOBAL_OPTIONS)
+
+  const image = document.querySelector('.jrdc-image').childNodes[0]
+
+  expect(image.src).toEqual(GLOBAL_OPTIONS.images[0])
+})
+
+test('Next and previous buttons are mounted', () => {
+  JrdCarousel('carousel', GLOBAL_OPTIONS)
+
+  const nextBtn = document.querySelector('.jrdc-button--right')
+  const prevBtn = document.querySelector('.jrdc-button--left')
+
+  expect(nextBtn).toBeTruthy()
+  expect(prevBtn).toBeTruthy()
+
+})
+
+test('Click on next button loads next image', () => {
+  JrdCarousel('carousel', GLOBAL_OPTIONS)
+
+  const nextBtn = document.querySelector('.jrdc-button--right')
+  nextBtn.click()
+  const imageBeingShown = document.querySelector('.jrdc-image').childNodes[0]
+
+  expect(imageBeingShown.src).toEqual(GLOBAL_OPTIONS.images[1])
+})
+
+test('Click on previous button loads last image', () => {
+  JrdCarousel('carousel', GLOBAL_OPTIONS)
+
+  const nextBtn = document.querySelector('.jrdc-button--left')
+  nextBtn.click()
+  const imageBeingShown = document.querySelector('.jrdc-image').childNodes[0]
+
+  expect(imageBeingShown.src).toEqual(GLOBAL_OPTIONS.images[GLOBAL_OPTIONS.images.length - 1])
+})
